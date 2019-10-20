7c9a0f9067adc7e790255009610d16ce5b07b7c8
melissaodonnell <55060254+melissaodonnell@users.noreply.github.com>
2019-10-20 13:08:08 -0700

    Pymongo Script for Stock Database
    Database project for CS-499 Final.

---
+++ 499DatabaseScript.py
@@ -0,0 +1,27 @@
+import json
+from bson import json_util
+from pymongo import MongoClient
+from pprint import pprint
+
+connection = MongoClient('localhost', 27017)
+db = connection['stocks499']
+collection = db['stocks']
+
+def read_document():
+  try:
+    key = raw_input("Enter key: ")
+    value = raw_input("Enter value: ")
+
+    cursor = collection.find_one({ key : value })
+	for document in cursor:
+		pprint(document)
+
+  except ValidationError as ve:
+    abort(400, str(ve))
+	
+def main():
+  read_document()
+
+main()
+
+input()
